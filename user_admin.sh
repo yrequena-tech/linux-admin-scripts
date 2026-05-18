befa484fcbc572f6d44b37761ce1b8b6b38d7c24
Yesenia Requena <yesenia.requena86@live.com>
2026-05-18 17:04:31 -0400

    Add user admin script - first commit

---
+++ user_admin.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#================================
+# User Administration Script
+# Author: Yesenia Requena
+# GitHub: yrequena-tech
+# Decsription: Automates user
+# creation, modification, deletion
+# ===============================
+
+
+echo "========================"
+echo " Linux User Admin Tool"
+echo "========================"
+echo "1. Create User"
+echo "2. Delete User"
+echo "3. Lock User"
+echo "4. Unlock User"
+echo "5. List All Users"
+echo "========================"
+echo -n "Choose an option: "
+read choice
+
+echo "You selected option: $choice"
+
+
